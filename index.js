42e482a6cb27b852b92440cfea82b84964a7a2f4
nick <nickagliano@gmail.com>
2020-07-22 12:51:31 -0400

    a little more commenting

--- index.js
+++ index.js
@@ -13,25 +13,25 @@ let util = new Utilities();
 // get command-line arguments, excluding 'npm' and 'start'
 let args = process.argv.slice(2);
 
+// if there's no command line arguments, calculate all sets
 if (args.length < 1) {
     args = ['1', '2', '3', '4', '5', '6'];
 }
 
-if (util.validateArgs(args)) {
-    for (let arg of args) {
-        util.printHeader(arg);
+if (util.validateArgs(args)) { // validate the user's arguments
+    for (let arg of args) { // iterate
+        util.printHeader(arg); // print which set is being processed
 
-        let set = new Set(arg); // build new set by passing the argument to constructor
+        let set = new Set(arg); // build a new Set object by passing the argument to constructor
 
         // get each day and calculate if it's a travel or full day
         //  and gracefully handle overlapping project days!
         //       a.k.a. do magic
         let days = set.getDayTypes();
     
-        // get reimbursements in a dollar amount
-        let reimbursements = util.sumReimbursements(days);
+        let reimbursements = util.sumReimbursements(days); // get reimbursement total in a dollar amount
 
-        util.printPrettyDays(days);
+        util.printPrettyDays(days); // print the processed days in a legible format
     
         console.log('\x1b[33m%s\x1b[0m', `\nReimbursement amount for Set #${arg}: $${reimbursements}\n`);
     }
